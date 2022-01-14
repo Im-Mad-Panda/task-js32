2a3059ffe5b5f3ff86a389d8839a1f235f8b7dab
Panda <traitnil@gmail.com>
2022-01-15 01:52:21 +0200

    multyply-table

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-function Cat(name, age, breed, color, isMale){
-    this.name;
-    this.age;
-    this.breed;
-    this.color;
-    this.isMale;
+'use strict'
+
+function getMultyplyTable(){
+    const table ={};
+    for (let i = 1; i < 10; i++) {
+        for (let j = 1; j < 10; j++) {
+            table[`${i} * ${j}`] = i * j;
+        }     
+    }
+        return table;
 }
-    const cat1 = new Cat('Barsik', 5, 'British', 'blue', true);
-    const cat2 = new Cat('Murka', 3, 'British', 'blue', false);
-
-    cat1.girfriend = cat2;
-
-    cat1.girfriend.name
+getMultyplyTable();
 
 
